e98fd55818b47873336ada85440ffd916e07bd89
Abdullahmad <kress1219@gmail.com>
2020-12-31 16:59:11 +0100

    code update

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-const jargon = document.querySelector('#imput');
+const positiveTab = document.querySelector('#positive');
+const negativeTabs = document.querySelector('#negative');
+const neutralTab = document.querySelector('#neutral');
+const tabLinks = document.querySelectorAll('.tablinks');
+const charges = document.querySelector('charge')
+negativeTabs.addEventListener('click', () => {
+    // removeActive();
+    negativeTabs.classList.add('active')
+})
+
+// const removeActive = () => {
+//     charges.forEach(element => {
+//         element.classList.remove('active')
+//     })
+// }
+
+function render() {
+
+}
